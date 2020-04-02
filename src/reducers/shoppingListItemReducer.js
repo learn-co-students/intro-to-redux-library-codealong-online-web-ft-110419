815328885609f00fe349d7fe9955ab79128b0694
LucyWeltner <lucyiweltner@gmail.com>
2020-04-02 12:35:26 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -6,11 +6,14 @@ export default function shoppingListItemReducer(
 ) {
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log(`Current length of the items list is ${state.items.length}`)
+			console.log(`Length of the items list will be ${state.items.length + 1}`)
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
 		default:
+			console.log(`Original length of the items list is ${state.items.length}`)
 			return state;
 	}
 }
